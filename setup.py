78015d62149a185589057615866033828f43b55c
juan-esteban-berger <juanestebanberger@gmail.com>
2024-04-11 16:10:42 -0600

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='termpandas',
-    version='0.0.4',
+    version='0.0.5',
     url='https://github.com/juan-esteban-berger/termpandas',
     author='Juan Esteban Berger',
     author_email='juanestebanberger@gmail.com',
